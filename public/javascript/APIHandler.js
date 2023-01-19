317fd4c858c948ae55782333ef96ea1ddca12caa
Sirah Tabarez <sartab2011@gmail.com>
2023-01-19 11:45:29 +0100

    iteration 1 & 2 updated

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,33 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+	constructor(baseUrl) {
+		this.api = axios.create({
+			baseURL: baseUrl
+		})
+	}
+
+	getFullList() {
+		return this.api
+				.get()
+				.then(result => console.log(result.data))
+				.catch(e => console.log(`Something went wrong getting all characters ${e}`));
   }
 
-  getFullList () {
-
+  getOneRegister (characterId) {
+    get(`/characters/${characterId}`)
+    .then(result => console.log(result.data))
+				.catch(e => console.log(`Something went wrong getting characters by ID ${e}`));
   }
 
-  getOneRegister () {
-
+  createOneRegister (characterInfo) {
+    return this.api.post(`/characters`, characterInfo);
   }
 
-  createOneRegister () {
-
+  updateOneRegister (characterId, characterInfo) {
+    return this.api.put(`/characters/${characterId}`, characterInfo);
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (characterId) {
+    return this.api.delete(`/characters/${characterId}`);
   }
 }
+
